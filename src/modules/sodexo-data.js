ba5c3c5530a85b5e57b2c4f423e46ed10d39e782
Eeli <eeli.klemettila@gmail.com>
2023-01-22 20:47:07 +0200

    fixes

--- src/modules/sodexo-data.js
+++
@@ -1,8 +0,0 @@
-import Menu from '../mock-data/sodexo.json';
-
-const coursesEn = Object.values(Menu.courses).map((course) => course.title_en);
-const coursesFi = Object.values(Menu.courses).map((course) => course.title_fi);
-
-const Sodexo = { coursesEn, coursesFi };
-
-export default Sodexo;
