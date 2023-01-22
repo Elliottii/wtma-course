1b515bac8c4e43a3f7d1433ff6d4ec4b9f4ab010
Eeli <eeli.klemettila@gmail.com>
2023-01-22 20:03:45 +0200

    task2 completed

--- src/index.js
+++ src/index.js
@@ -1,111 +1,33 @@
-const coursesEn = [
-  'Hamburger, cream sauce and poiled potates',
-  'Goan style fish curry and whole grain rice',
-  'Vegan Chili sin carne and whole grain rice',
-  'Broccoli puree soup, side salad with two napas',
-  'Lunch baguette with BBQ-turkey filling',
-  'Cheese / Chicken / Vege / Halloum burger and french fries',
+const courses = [
+  { name: 'Lingonberry jam', price: 4.0 },
+  { name: 'Mushroom and bean casserole', price: 5.5 },
+  { name: 'Chili-flavoured wheat', price: 3.0 },
+  { name: 'Vegetarian soup', price: 4.8 },
+  { name: 'Pureed root vegetable soup with smoked cheese', price: 8.0 },
 ];
-const coursesFi = [
-  'Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa',
-  'Goalaista kalacurrya ja täysjyväriisiä',
-  'Vegaaninen Chili sin carne ja täysjyväriisi',
-  'Parsakeittoa,lisäkesalaatti kahdella napaksella',
-  'Lounaspatonki BBQ-kalkkuna täytteellä',
-  'Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset',
-];
+const originalCourses = [...courses];
 
-let lang = 'fi';
-let activeMenu = coursesFi;
-let order = '';
+let course = 'Mushroom and bean casserole';
 
-/**
- * Renders menu content to html page
- * @param {Array} menu - array of dishes
- */
-const renderMenu = (menu) => {
-  const menuBox = document.querySelector('.menu');
-  menuBox.innerHTML = '';
-  const menuList = document.createElement('ul');
-
-  for (const dish of menu) {
-    const li = document.createElement('li');
-    li.textContent = dish;
-    menuList.append(li);
-  }
-  menuBox.append(menuList);
+const validateCourse = (course) => {
+  const regexpPattern =
+    /^[A-ZÖÄÅ][\s\d a-zöäå \_\-\.\:\;\(\)\{\}\\\/\[\] ]{3,63}$/;
+  return regexpPattern.test(course);
 };
+console.log('RegExp test ->', 'String:', course, validateCourse(course));
 
-renderMenu(activeMenu);
+console.log('Original courses:', originalCourses);
 
-/**
- * Sort menu alphabetically
- * @param {Array} menu - Array of dishes
- * @param {string} order - "asc" or "desc"
- * @returns sorted menu array
- */
-const sortMenu = (menu, order) => {
-  if (order === 'desc') {
-    menu.reverse();
-  } else {
-    menu.sort();
-  }
-  return menu;
-};
-
-/**
- * Change UI language
- * @param {string} language
- */
-const changeLanguage = (language) => {
-  if (language === 'fi') {
-    activeMenu = coursesFi;
-    document.querySelector('#title').innerHTML = 'Ruokalista';
-    document.querySelector('#sort').innerHTML = 'Järjestä';
-    document.querySelector('#fi-en').innerHTML = 'Vaihda kieli';
-    document.querySelector('#random').innerHTML = 'Valitse satunnainen ateria';
-  } else if (language === 'en') {
-    activeMenu = coursesEn;
-    document.querySelector('#title').innerHTML = 'Menu';
-    document.querySelector('#sort').innerHTML = 'Sort';
-    document.querySelector('#fi-en').innerHTML = 'Change language';
-    document.querySelector('#random').innerHTML = 'Pick random dish';
-  }
-  lang = language;
-  renderMenu(activeMenu);
-};
-
-/**
- * Get a random item from an array of dishes
- * @param {Array} menu -Array of dishes
- * @returns random dish item
- */
-const getRandomDish = (menu) => {
-  const randomIndex = Math.floor(Math.random() * menu.length);
-  return menu[randomIndex];
-};
-
-const sortButton = document.querySelector('#sort');
-sortButton.addEventListener('click', () => {
-  if (order === 'asc') {
-    order = 'desc';
-  } else {
-    order = 'asc';
-  }
-  renderMenu(sortMenu(activeMenu, order));
+const sortedCourses = courses.sort((a, b) => {
+  return b.price - a.price;
 });
+console.log('Sorted by price:', sortedCourses);
 
-const changeLanguageButton = document.querySelector('#fi-en');
-changeLanguageButton.addEventListener('click', () => {
-  if (lang === 'fi') {
-    lang = 'en';
-  } else {
-    lang = 'fi';
-  }
-  changeLanguage(lang);
-});
+const filteredCourses = courses.filter((course) => course.price < 5);
+console.log('Filtered prices under 5€:', filteredCourses);
 
-const randomDishButton = document.querySelector('#random');
-randomDishButton.addEventListener('click', () => {
-  document.querySelector('#random-dish').innerHTML = getRandomDish(activeMenu);
-});
+const totalPriceCourses = courses.reduce(
+  (acc, courses) => acc + courses.price,
+  0
+);
+console.log('All courses total price:', totalPriceCourses);
