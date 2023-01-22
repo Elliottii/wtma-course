c782cea9214d31508f8f121896651c20e8b0d123
Eeli <eeli.klemettila@gmail.com>
2023-01-22 15:21:14 +0200

    task2 start

--- src/index.js
+++ src/index.js
@@ -1,183 +1,111 @@
-const minNumber = 1;
-const maxNumber = 1000000000;
-const maxGuess = 100;
+const coursesEn = [
+  'Hamburger, cream sauce and poiled potates',
+  'Goan style fish curry and whole grain rice',
+  'Vegan Chili sin carne and whole grain rice',
+  'Broccoli puree soup, side salad with two napas',
+  'Lunch baguette with BBQ-turkey filling',
+  'Cheese / Chicken / Vege / Halloum burger and french fries',
+];
+const coursesFi = [
+  'Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa',
+  'Goalaista kalacurrya ja täysjyväriisiä',
+  'Vegaaninen Chili sin carne ja täysjyväriisi',
+  'Parsakeittoa,lisäkesalaatti kahdella napaksella',
+  'Lounaspatonki BBQ-kalkkuna täytteellä',
+  'Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset',
+];
 
-let startTime = '';
+let lang = 'fi';
+let activeMenu = coursesFi;
+let order = '';
 
-document.getElementById('min').innerHTML = minNumber;
-document.getElementById('max').innerHTML = maxNumber;
-document.getElementById('maxGuess').innerHTML = maxGuess;
+/**
+ * Renders menu content to html page
+ * @param {Array} menu - array of dishes
+ */
+const renderMenu = (menu) => {
+  const menuBox = document.querySelector('.menu');
+  menuBox.innerHTML = '';
+  const menuList = document.createElement('ul');
 
-let randomNumber = Math.floor(
-  Math.random() * (maxNumber - minNumber + 1) + minNumber
-);
-
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessAlgorithm = document.querySelector('.algorithmSubmit');
-const guessField = document.querySelector('.guessField');
-const testAlgorithm = document.querySelector('.testAlgorithmSubmit');
-const testResults = document.querySelector('.testResults');
-
-let guessCount = 1;
-let resetButton;
-
-function checkGuess() {
-  const userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    startTime = Date.now();
-    guesses.textContent = 'Previous guesses: ';
+  for (const dish of menu) {
+    const li = document.createElement('li');
+    li.textContent = dish;
+    menuList.append(li);
   }
-  guesses.textContent += `${userGuess}, `;
+  menuBox.append(menuList);
+};
 
-  if (userGuess === randomNumber) {
-    finalTime = (Date.now() - startTime) / 1000;
-    lastResult.textContent =
-      'Congratulations! You got it right in ' +
-      guessCount +
-      ' try and it took ' +
-      finalTime +
-      ' seconds';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    setGameOver();
-  } else if (guessCount === maxGuess) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-    lowOrHi.textContent = '';
-    setGameOver();
+renderMenu(activeMenu);
+
+/**
+ * Sort menu alphabetically
+ * @param {Array} menu - Array of dishes
+ * @param {string} order - "asc" or "desc"
+ * @returns sorted menu array
+ */
+const sortMenu = (menu, order) => {
+  if (order === 'desc') {
+    menu.reverse();
   } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
-    if (userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if (userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
-    }
+    menu.sort();
   }
+  return menu;
+};
 
-  guessCount++;
-  guessField.value = '';
-  guessField.focus();
-}
-guessSubmit.addEventListener('click', checkGuess);
-
-function setGameOver() {
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  guessAlgorithm.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
-  document.body.append(resetButton);
-  resetButton.addEventListener('click', resetGame);
-}
-
-function resetGame() {
-  min = minNumber;
-  max = maxNumber;
-  mid = Math.round(max + min - 1) / 2;
-  guessCount = 1;
-  startTime = Date.now();
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (const resetPara of resetParas) {
-    resetPara.textContent = '';
-  }
-
-  resetButton.parentNode.removeChild(resetButton);
-
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessAlgorithm.disabled = false;
-  guessField.value = '';
-  guessField.focus();
-
-  lastResult.style.backgroundColor = 'white';
-
-  randomNumber = Math.floor(
-    Math.random() * (maxNumber - minNumber + 1) + minNumber
-  );
-}
-
-let min = minNumber;
-let max = maxNumber;
-let mid = Math.round(max + min - 1) / 2;
 /**
- * Algorithm always guess the number at the middle.
- * If the guess is too low, the new minium number is the previous guess +1.
- * If the quess is too high, the new maxium number is the previous quess -1.
- * Each guess sets either new min or max and the next guess is the middle of those values.
+ * Change UI language
+ * @param {string} language
  */
-function numberGuessingAlgorithm() {
-  startTime = Date.now();
-  while (guessCount < maxGuess && mid != randomNumber) {
-    guessField.value = mid;
-    checkGuess();
-    if (mid > randomNumber) {
-      max = mid - 1;
-      mid = Math.round((max + min - 1) / 2);
-    } else if (mid < randomNumber) {
-      min = mid + 1;
-      mid = Math.round((max + min + 1) / 2);
-    }
+const changeLanguage = (language) => {
+  if (language === 'fi') {
+    activeMenu = coursesFi;
+    document.querySelector('#title').innerHTML = 'Ruokalista';
+    document.querySelector('#sort').innerHTML = 'Järjestä';
+    document.querySelector('#fi-en').innerHTML = 'Vaihda kieli';
+    document.querySelector('#random').innerHTML = 'Valitse satunnainen ateria';
+  } else if (language === 'en') {
+    activeMenu = coursesEn;
+    document.querySelector('#title').innerHTML = 'Menu';
+    document.querySelector('#sort').innerHTML = 'Sort';
+    document.querySelector('#fi-en').innerHTML = 'Change language';
+    document.querySelector('#random').innerHTML = 'Pick random dish';
   }
-  guessField.value = mid;
-  checkGuess();
-}
-let testCount = 0;
-let testGuesses = [];
+  lang = language;
+  renderMenu(activeMenu);
+};
 
-let testRandomNumber = Math.floor(
-  Math.random() * (maxNumber - minNumber + 1) + minNumber
-);
 /**
- * Algorithm guess the number multiple times and saves the guess count to an array.
- * After that algorithm calculates max, min and avg values of the array.
+ * Get a random item from an array of dishes
+ * @param {Array} menu -Array of dishes
+ * @returns random dish item
  */
-function testingAlgorithm() {
-  startTime = Date.now();
-  num = Number(testMultiplier.value);
-  while (num > 0) {
-    if (mid > testRandomNumber) {
-      max = mid - 1;
-      mid = Math.round((max + min - 1) / 2);
-    } else if (mid < testRandomNumber) {
-      min = mid + 1;
-      mid = Math.round((max + min + 1) / 2);
-    } else if (mid === testRandomNumber) {
-      testGuesses.push(testCount);
-      testCount = 0;
-      min = minNumber;
-      max = maxNumber;
-      mid = Math.round(max + min - 1) / 2;
-      testRandomNumber = Math.floor(
-        Math.random() * (maxNumber - minNumber + 1) + minNumber
-      );
-      num -= 1;
-    }
+const getRandomDish = (menu) => {
+  const randomIndex = Math.floor(Math.random() * menu.length);
+  return menu[randomIndex];
+};
 
-    testCount += 1;
+const sortButton = document.querySelector('#sort');
+sortButton.addEventListener('click', () => {
+  if (order === 'asc') {
+    order = 'desc';
+  } else {
+    order = 'asc';
   }
-  finalTime = (Date.now() - startTime) / 1000;
-  let sum = testGuesses.reduce((acc, current) => acc + current);
-  console.log(testGuesses);
-  testResults.textContent =
-    '"AI" guessed the number right ' +
-    testMultiplier.value +
-    ' times in ' +
-    finalTime +
-    ' seconds. Maximum guesses ' +
-    Math.max(...testGuesses) +
-    ', minimum guesses ' +
-    Math.min(...testGuesses) +
-    ' and average ' +
-    sum / testGuesses.length +
-    ' to get it right.';
-  testResults.style.backgroundColor = 'green';
-  testMultiplier.value = '';
-  testGuesses = [];
-}
+  renderMenu(sortMenu(activeMenu, order));
+});
 
-guessAlgorithm.addEventListener('click', numberGuessingAlgorithm);
-testAlgorithm.addEventListener('click', testingAlgorithm);
+const changeLanguageButton = document.querySelector('#fi-en');
+changeLanguageButton.addEventListener('click', () => {
+  if (lang === 'fi') {
+    lang = 'en';
+  } else {
+    lang = 'fi';
+  }
+  changeLanguage(lang);
+});
+
+const randomDishButton = document.querySelector('#random');
+randomDishButton.addEventListener('click', () => {
+  document.querySelector('#random-dish').innerHTML = getRandomDish(activeMenu);
+});
